07f99b1f2995a16d7739165efeba195804de099d
Taha Selim Bebek <tsbebek@gmail.com>
2013-03-20 10:40:32 -0700

    Project upload

---
+++ instagrampopular/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  instagrampopular
+//
+//  Created by Bebek, Taha on 10/21/12.
+//  Copyright (c) 2012 Bebek, Taha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong,nonatomic) UIWindow *window;
+
+@end
---
+++ instagrampopular/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  instagrampopular
+//
+//  Created by Bebek, Taha on 10/21/12.
+//  Copyright (c) 2012 Bebek, Taha. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
